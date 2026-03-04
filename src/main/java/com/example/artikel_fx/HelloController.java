9dd7379df70e4e9a3f9db5fdfbc2ba78a7cff243
Jalal Sbai <j.sbai@gmx.de>
2026-03-04 20:51:45 +0100

    Fix: UI configuration and controller logic

--- src/main/java/com/example/artikel_fx/HelloController.java
+++ src/main/java/com/example/artikel_fx/HelloController.java
@@ -1,13 +1,35 @@
 package com.example.artikel_fx;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableView<Artikel> tableView;
+
+    @FXML
+    private TableColumn<Artikel, String> nameColumn;
+
+    @FXML
+    private TableColumn<Artikel, Double> priceColumn;
+
+    private final ObservableList<Artikel> artikelListe = FXCollections.observableArrayList();
+
+    @FXML
+    public void initialize() {
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        tableView.setItems(artikelListe);
+    }
+
+    @FXML
+    protected void addArtikel() {
+        artikelListe.add(new Artikel(1, "Neuer Artikel", 9.99, 1));
     }
 }
